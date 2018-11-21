c079139c23395e66e85c04e975d786c7d0d0d57e
yang.liu <yang.liu@hypers.com>
2018-11-21 19:42:00 +0800

    feat: 增加列表渲染

--- src/components/menus.vue
+++ src/components/menus.vue
@@ -2,13 +2,15 @@
   <!-- 菜单容器 -->
   <div class="list-todo">
     <!--单个菜单容器 -->
-    <a class="list-todo activeListClass list">
+    <!-- v-for 列表渲染 -->
+    <a class="list-todo list activeListClass" v-for="item in items" :key="item.title">
       <!--锁的图标 -->
-      <span class="icon-lock"></span>
+      <!-- v-if 条件渲染-->
+      <span class="icon-lock" v-if="item.locked"></span>
       <!-- 数字 -->
-      <span class="count-list">1</span>
-      <!-- 菜单内容 -->
-      星期一
+      <span class="count-list" v-if="item.count > 0"> {{item.count}}</span>
+      {{item.title}}
+      <br/>
     </a>
     <!-- 新增菜单 -->
     <a class="link-list-new">
@@ -19,7 +21,16 @@
 
 <script>
 export default {
-  name: 'menus.vue'
+  name: 'menus.vue',
+  // data 函数
+  data () {
+    return {
+      items: [{title: '星期一', count: 3, locked: true}, // 菜单模拟数据
+        {title: '星期二', count: 1, locked: true},
+        {title: '星期三', count: 2, locked: true}
+      ]
+    }
+  }
 }
 </script>
 
